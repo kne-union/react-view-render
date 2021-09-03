c6bd9257e3ed77c7889ef3c70f87a8cb93b7eebd
林志鹏 <zhipenglin@knx.com.cn>
2021-09-03 10:23:13 +0800

    删除无用文件

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,17 @@
 import ViewRender from '@kne/react-view-render';
 import {message, Modal} from 'antd';
+import {ConfigProvider} from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
 
 const App = () => {
-    return <ViewRender lib={{
-        message,
-        modal: {
-            confirm: Modal.confirm
-        }
-    }} url="/resume/index.json"/>;
+    return <ConfigProvider autoInsertSpaceInButton={false} locale={zhCN}>
+        <ViewRender lib={{
+            message,
+            modal: {
+                confirm: Modal.confirm
+            }
+        }} url="/resume/index.json"/>
+    </ConfigProvider>;
 };
 
 export default App;
--- src/Logic/List/index.js
+++ src/Logic/List/index.js
@@ -1,18 +1,19 @@
-import React, { Children } from 'react';
+import React, {Children} from 'react';
 import Context from '../Context';
 import classnames from 'classnames';
-import { applyVariable } from '../../util';
+import {applyVariable} from '../../util';
 
-const List = ({ resource, map = (value) => value, children, className, ...props }) => {
-  return <div {...props} className={classnames(className, 'view-render-list')}>{(resource || []).map((item, index) => {
-    const props = map({ $item: item, $index: index });
-    return Children.map(children, (item) => {
-      if (!item) {
-        return item;
-      }
-      return <Context variable={props}>{children}</Context>;
-    });
-  })}</div>;
+const List = ({resource, map = (value) => value, empty = null, children, className, ...props}) => {
+    return <div {...props}
+                className={classnames(className, 'view-render-list')}>{(resource || []).length === 0 ? empty : (resource || []).map((item, index) => {
+        const props = map({$item: item, $index: index});
+        return Children.map(children, (item) => {
+            if (!item) {
+                return item;
+            }
+            return <Context variable={props}>{children}</Context>;
+        });
+    })}</div>;
 };
 
 export default applyVariable(List);
