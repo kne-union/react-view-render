cb5747ae8a27c861fbbecb2017ee13adee4b7b74
wen1 <571047994@qq.com>
2021-12-10 17:51:57 +0800

    加个选择器

---
+++ src/Form/DateRangePickerToday/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { DatePicker } from '@kne/react-form-antd';
+import fieldDecorator from '../fieldDecorator';
+
+export default fieldDecorator(DatePicker.TodayPicker);
